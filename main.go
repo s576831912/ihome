7e79facbef4f20f7844ee622b521412ba9d62a76
MrWnag <934105927@qq.com>
2019-11-10 18:34:52 -0800

    添加数据库

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"ihomebj5q/model"
+	"fmt"
 )
 
 func main() {
 	router:=gin.Default()
 	r1:=router.Group("/v1")
+	err:=model.InitDb()
+	if err != nil{
+		//将错误打印到日志文件中
+		fmt.Println(err)
+		return
+	}
 	{
 		r1.GET("/efg", func(ctx *gin.Context) {
 			ctx.Writer.WriteString("adbcdw")
@@ -20,7 +27,6 @@ func main() {
 		})
 	}
 
-	model.InitModel()
 	router.Run(":8099")
 
 }
